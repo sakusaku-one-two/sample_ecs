38579431db5785b62cf2fd466310fc890c65df64
apple <apple@appurunoMacBook-Pro.local>
2025-06-01 12:40:07 +0900

    sampel fetch

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<string>("get dta");
+
+
+
+  useEffect(()=>{
+    const get = async () => {
+      try {
+        const result = await fetch(`${window.location.host}/api/`,{
+            method : "POST"
+        });
+
+        const textData = await result.text();
+        setData(textData)
+      } catch(e:any) {
+        setData(e.message);
+      }
+    };
+  
+
+    get()
+  },[]);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className=''>
+          POST --> {data}
+          
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
