5278a8550eb92dde0641da1b344702d0f050964e
apple <apple@appurunoMacBook-Pro.local>
2025-06-03 15:13:37 +0900

    up

--- api/api_1/internal/handlers/handler.go
+++ api/api_1/internal/handlers/handler.go
@@ -11,7 +11,12 @@ func ReflectMessage(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("POST以外のメソッドが呼ばれました", r.Method)
 		return
 	}
-
-	w.Write([]byte{"sdfadsfsasf"})
+	var body_byte []byte
+	_, err := r.Body.Read(body_byte)
+	if err != nil {
+		w.Write([]byte("読み取りに失敗しました"))
+		return
+	}
+	w.Write(body_byte)
 
 }
